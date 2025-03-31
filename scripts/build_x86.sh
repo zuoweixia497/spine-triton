ffc7d388cc40f78c87b141313eaf4b95e9f2cee0
huangjinghui <jinghui.huang@spacemit.com>
2025-03-31 08:48:49 +0000

    update build scripts
    Change-Id: I97332b1f3836d551d66840579d2c8ef0d74aa316

--- scripts/build_x86.sh
+++ scripts/build_x86.sh
@@ -7,7 +7,20 @@ export TRITON_PLUGIN_DIRS=${PWD}
 
 pushd triton/python
 
-TRITON_BUILD_PROTON=false TRITON_BUILD_WITH_CLANG_LLD=true TRITON_BUILD_WITH_CCACHE=false LLVM_ROOT_DIR=${LLVM_INSTALL_DIR} \
+TRITON_BUILD_PROTON=false TRITON_BUILD_WITH_CLANG_LLD=false \
+    TRITON_BUILD_WITH_CCACHE=false LLVM_ROOT_DIR=${LLVM_INSTALL_DIR} MAX_JOBS=20 \
     python3 setup.py build_py
+
+popd
+
+mkdir -p build
+
+pushd build
+
+cp -r ../triton/python/build/lib*/triton ./
+
+cp ../triton/python/build/cmake*/third_party/triton_shared/tools/triton-shared-opt/triton-shared-opt ./
+
+cp -r ${TRITON_PLUGIN_DIRS}/backend ./triton/triton_shared
 
 popd
