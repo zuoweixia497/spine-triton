bd1184a54cca8f821e5660816c4f075e417233c9
huangjinghui <jinghui.huang@spacemit.com>
2025-04-03 09:31:58 +0000

    add pow and tanh for tt ast
    Change-Id: Id6746560c6d92c9c5c42d6321664790a5d25bf88

--- language/cpu/libdevice.py
+++ language/cpu/libdevice.py
@@ -1,20 +1,31 @@
 from triton.language import core
 
+@core.extern
+def erf(arg0, _builder=None):
+    return core.tensor(_builder.create_erf(arg0.handle), arg0.type)
 
-def erf(arg0):
-    ...
+@core.extern
+def pow(arg0, arg1, _builder=None):
+    return core.extern_elementwise(
+        "", "", [arg0, arg1], {
+            (core.dtype("fp32"), core.dtype("fp32")): ("__rvv_powf", core.dtype("fp32")),
+            (core.dtype("fp64"), core.dtype("fp64")): ("__rvv_powd", core.dtype("fp64")),
+        }, is_pure=True, _builder=_builder)
 
-def pow(arg0, arg1):
-    ...
-
-def exp(arg0):
-    ...
+@core.extern
+def exp(arg0, _builder=None):
+    return core.tensor(_builder.create_exp(arg0.handle), arg0.type)
 
 def exp2(arg0):
     ...
 
-def tanh(arg0):
-    ...
+@core.extern
+def tanh(arg0, _builder=None):
+    return core.extern_elementwise(
+        "", "", [arg0], {
+            (core.dtype("fp32"), ): ("__rvv_tanhf", core.dtype("fp32")),
+            (core.dtype("fp64"), ): ("__rvv_tanhd", core.dtype("fp64")),
+        }, is_pure=True, _builder=_builder)
 
 def trunc(arg0):
     ...
