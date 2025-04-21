a2cc8cd4804e10282dfba52c5aa26feb5682bddc
huangjinghui <jinghui.huang@spacemit.com>
2025-04-21 02:25:44 +0000

    add flag gems example for triton shared
    Change-Id: I5cb2acb59dc379154867cd74b3217f7cf0307bf9

---
+++ examples/flag_gems_bmm.py
@@ -0,0 +1,21 @@
+import time
+import numpy as np
+from functools import wraps
+import torch
+import triton
+from triton.backends.triton_shared.driver import CPUDriver
+triton.runtime.driver.set_active(CPUDriver())
+import flag_gems
+
+if __name__ == "__main__":
+    M, N, K = 256, 256, 512
+    A = torch.randn((4, M, K), dtype=torch.float32, device=flag_gems.device, requires_grad=False)
+    B = torch.randn((4, K, N), dtype=torch.float32, device=flag_gems.device, requires_grad=False)
+
+    with flag_gems.use_gems():
+        with torch.no_grad():
+            C = torch.bmm(A, B)
+
+    C_ref = torch.bmm(A, B)
+
+    torch.testing.assert_close(C, C_ref, atol=1e-2, rtol=0)
---
+++ examples/flag_gems_gelu.py
@@ -0,0 +1,20 @@
+import time
+import numpy as np
+from functools import wraps
+import torch
+import triton
+from triton.backends.triton_shared.driver import CPUDriver
+triton.runtime.driver.set_active(CPUDriver())
+import flag_gems
+
+if __name__ == "__main__":
+    M, K = 256, 512
+    approximate = "tanh"
+    inp = torch.randn([M, K], dtype=torch.float32, device=flag_gems.device, requires_grad=False)
+    with flag_gems.use_gems():
+        with torch.no_grad():
+            C = torch.nn.functional.gelu(inp, approximate=approximate)
+
+    C_ref = torch.nn.functional.gelu(inp, approximate=approximate)
+
+    torch.testing.assert_close(C, C_ref, atol=1e-2, rtol=0)
