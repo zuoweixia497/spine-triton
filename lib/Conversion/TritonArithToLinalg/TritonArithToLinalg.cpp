b1304ce04e847430fb3f9b241c89c1e459236ecb
root <weixia.zuo@spacemit.com>
2025-04-23 10:39:15 +0000

    linalg gelu_none lower optimize
    Change-Id: Ie44dd7775fb98520113edb0f89b92a1dcb158b58

--- lib/Conversion/TritonArithToLinalg/TritonArithToLinalg.cpp
+++ lib/Conversion/TritonArithToLinalg/TritonArithToLinalg.cpp
@@ -91,6 +91,7 @@ void mlir::triton::populateTritonArithToLinalgConversionPatterns(
   patterns.add<ArgMaxConverter>(patterns.getContext());
   patterns.add<ReduceConverter>(patterns.getContext());
   patterns.add<ConvertExternGeluThan>(patterns.getContext());
+  patterns.add<ConvertExternGeluNone>(patterns.getContext());
   // Note: the ordering here matters!
   // These patterns are added last to they will be tried last.
   linalg::populateElementwiseToLinalgConversionPatterns(patterns);
